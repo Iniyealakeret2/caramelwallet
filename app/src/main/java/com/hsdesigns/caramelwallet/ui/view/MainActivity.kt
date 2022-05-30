df10435ad300f23acc5e77bcfa489e417012e261
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-05-30 13:53:44 +0100

    created custom buttons

--- app/src/main/java/com/hsdesigns/caramelwallet/ui/view/MainActivity.kt
+++ app/src/main/java/com/hsdesigns/caramelwallet/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hsdesigns.caramelwallet
+package com.hsdesigns.caramelwallet.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hsdesigns.caramelwallet.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
