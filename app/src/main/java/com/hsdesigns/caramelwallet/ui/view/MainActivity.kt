45e76283862ba5472f2a09d6b0fff52fd1bab9ce
Iniyealakeret2 <iniyealakeret1@gmail.com>
2022-06-01 12:57:39 +0100

    bottom naviagtion created

--- app/src/main/java/com/hsdesigns/caramelwallet/ui/view/MainActivity.kt
+++ app/src/main/java/com/hsdesigns/caramelwallet/ui/view/MainActivity.kt
@@ -2,11 +2,24 @@ package com.hsdesigns.caramelwallet.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hsdesigns.caramelwallet.R
+import com.hsdesigns.caramelwallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpBottomNavigation()
+    }
+    private fun setUpBottomNavigation(){
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
 }
